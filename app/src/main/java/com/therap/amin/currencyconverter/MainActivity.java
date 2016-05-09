f7092498fabb0a4988c89faa6b1e4d9fe362c4a4
ripon46 <amin@therapservices.net>
2016-05-09 14:51:33 +0600

    comment deleted

--- app/src/main/java/com/therap/amin/currencyconverter/MainActivity.java
+++ app/src/main/java/com/therap/amin/currencyconverter/MainActivity.java
@@ -6,21 +6,15 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DecimalFormat;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
